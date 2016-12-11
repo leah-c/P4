76634ba7e88ba7c73baff46ab29ee1dcda427276
LeahC <chan.leah@gmail.com>
2016-12-11 14:39:29 -0500

    created master layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        {{-- Yield the title if it exists, otherwise default to 'Foobooks' --}}
+        @yield('title', 'Expense Tracker')
+    </title>
+
+    <meta charset='utf-8'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" >
+
+    <link href="/css/style.css" type='text/css' rel='stylesheet'>
+    {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+    @yield('head')
+
+</head>
+<body>
+
+  <div class="container">
+    <header>
+      <h1 id="app_name">Expense Tracker</h1>z
+    </header>
+
+    <section>
+        {{-- Main page content will be yielded here --}}
+        @yield('content')
+    </section>
+
+    <footer>
+    </footer>
+
+    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+    @yield('body')
+  </div>
+
+</body>
+</html>
