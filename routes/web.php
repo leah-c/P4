4b40f78dddb982b2ee6e402a4276a8cbd8a65817
LeahC <chan.leah@gmail.com>
2016-12-11 22:09:57 -0500

    created route to direct users to sign in/ registration page

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,28 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
+/*
 Route::get('/', function () {
     //return view('home');
     return view('index');
 
 });
+*/
+# Direct user to the Expense Tracker landing page
+Route::get('/', 'IndexController@index')->name('expense_tracker.index');
+/*
+# Direct user to the Generator tools landing page
+Route::get('/', 'DEVBFF@home')->name('gen_home.index');
+
+# Direct user to the ipsum text generator landing page
+Route::get('/generate_ipsum', 'DEVBFF@ipsumHome')->name('gen_ipsum.index');
+
+# Direct user to the user generator landing page
+Route::get('/generate_users', 'DEVBFF@userHome')->name('gen_user.index');
+
+# Directs user to the confirmation page of the ipsum generator
+Route::post('/generate_ipsum', 'DEVBFF@createIpsum')->name('gen_ipsum.create');
+
+# Directs user to the confirmation page of the random user generator
+Route::post('/generate_users', 'DEVBFF@createUsers')->name('gen_user.create');
+*/
