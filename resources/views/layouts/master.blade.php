9b751cbc54ab980895a0303aa974d6c66725c71d
LeahC <chan.leah@gmail.com>
2016-12-11 19:49:43 -0500

    added link to bootstrap font-awesome

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    
     <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" >
 
     <link href="/css/style.css" type='text/css' rel='stylesheet'>
@@ -21,7 +23,7 @@
 
   <div class="container">
     <header>
-      <h1 id="app_name">Expense Tracker</h1>
+      <!--<h1 id="app_name">Expense Tracker</h1>-->
     </header>
 
     <section>
