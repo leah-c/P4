8f3a3b83b91b3e1d635577d9a489423d613a9a0c
LeahC <chan.leah@gmail.com>
2016-12-14 17:31:43 -0500

    added code to test db connection

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,41 @@ Route::post('/home', 'IndexController@homepage')->name('expense_tracker.home');
 # Directs form input from the account registration page to be validated
 Route::post('/validate_acct', 'AccountController@validateAccount')->name('validate_acct.create');
 #Route::post('/home', 'AccountController@validateAccount')->name('validate_acct.create');
+
+/*
+#test db conection
+Route::get('/debug', function() {
+
+  echo '<pre>';
+
+  echo '<h1>Environment</h1>';
+  echo App::environment().'</h1>';
+
+  echo '<h1>Debugging?</h1>';
+  if(config('app.debug')) echo "Yes"; else echo "No";
+
+  echo '<h1>Database Config</h1>';
+  /*
+  The following line will output your MySQL credentials.
+  Uncomment it only if you're having a hard time connecting to the database and you
+  need to confirm your credentials.
+  When you're done debugging, comment it back out so you don't accidentally leave it
+  running on your live server, making your credentials public.
+  */
+  //print_r(config('database.connections.mysql'));
+/*
+  echo '<h1>Test Database Connection</h1>';
+  try {
+    $results = DB::select('SHOW DATABASES;');
+    echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+    echo "<br><br>Your Databases:<br><br>";
+    print_r($results);
+  }
+  catch (Exception $e) {
+    echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+  }
+
+  echo '</pre>';
+
+});
+*/
