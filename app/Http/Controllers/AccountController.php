9908eb696a15f3d4dab6ac16ebe4087d14f493cc
LeahC <chan.leah@gmail.com>
2016-12-13 23:05:44 -0500

    created validateAccount()

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -6,80 +6,114 @@ use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function registerAccount()
+  {
+    # directs user to the account registration page
+    return view('account_reg');
+  }
+
+  public function validateAccount(Request $request)
+  {
+    dd($request);
+    # Validation
+    /*this->validate($request, [
+     'name' => 'required|numeric|min:1|max:50',
+     'password' => 'required|numeric|min:1|max:50',
+     'confirm_password' => 'required|numeric|min:1|max:50',
+   ])
      */
-    public function registerAccount()
-    {
-        # directs user to the account registration page
-        return view('account_reg');
+
+      //$input = $request->all();
+      //ddump($request);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+/*    public function createUserAccount(Request $request)
     {
-        //
-    }
+      # Validation
+      $this->validate($request, [
+        'numParagraphs' => 'required|numeric|min:1|max:50',
+      ]);
+*/
+    /*
+    # generate paragraphs
+    $numParagraphs = $request->input('numParagraphs');
+    $generator = new \Badcow\LoremIpsum\Generator();
+    $paragraphs = $generator->getParagraphs($numParagraphs);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return view('generator.ipsum_confirm')
+    ->with('numParagraphs', $numParagraphs)
+    ->with(compact('paragraphs'));
+    */
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+  * Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function edit($id)
+  {
+    //
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id)
+  {
+    //
+  }
 }
