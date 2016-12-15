1c237710bdd16b720c08db3cbad879bd7f4ecc59
LeahC <chan.leah@gmail.com>
2016-12-15 15:59:29 -0500

    updated button look and feel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,39 +2,39 @@
 
 
 @section('title')
-  Expense Tracker
+Expense Tracker
 @endsection
 
 
 {{--
-This `head` section will be yielded right before the closing </head> tag.
-Use it to add specific things that *this* View needs in the head,
-such as a page specific stylesheets.
---}}
-@section('head')
+  This `head` section will be yielded right before the closing </head> tag.
+  Use it to add specific things that *this* View needs in the head,
+  such as a page specific stylesheets.
+  --}}
+  @section('head')
 
-@endsection
+  @endsection
 
 
-@section('content')
+  @section('content')
 
   <p>Welcome to the expense tracker tool menu!</p>
 
   <h2>Let's Get Started!</h2>
   <p> Select from one of the following options. </p>
   <ul>
-    <li class="expenseOp"><a href="#">Add an Expense</a></li>
-    <li class="expenseOp"><a href="#">Delete an Expense</a></li>
-    <li class="expenseOp"><a href="#">Update an Expense</a></li>
-    <li class="expenseOp"><a href="#">View Expenses</a></li>
+    <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a></li>
+    <li class="expenseOp"><a href="#" class="btn btn-sm btn-danger btn-block" id="delete_expense"><span class="glyphicon glyphicon-remove"></span> Delete an Expense</a></li>
+    <li class="expenseOp"><a href="#" class="btn btn-sm btn-warning btn-block" id="edit_expense"><span class="glyphicon glyphicon-edit"></span> Edit an Expense</a></li>
+    <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="view_expense"><span class="glyphicon glyphicon-th-list"></span> View Expenses</a></li>
   </ul>
+  
+  @endsection
 
-@endsection
-
-{{--
-This `body` section will be yielded right before the closing </body> tag.
-Use it to add specific things that *this* View needs at the end of the body,
-such as a page specific JavaScript files.
---}}
-@section('body')
-@endsection
+  {{--
+    This `body` section will be yielded right before the closing </body> tag.
+    Use it to add specific things that *this* View needs at the end of the body,
+    such as a page specific JavaScript files.
+    --}}
+    @section('body')
+    @endsection
