de4c400ab5a5311a12b0bcf5fc53b3bca0fc4ef0
LeahC <chan.leah@gmail.com>
2016-12-15 00:51:15 -0500

    created dummy data for expenses table seeder

--- database/seeds/ExpensesTableSeeder.php
+++ database/seeds/ExpensesTableSeeder.php
@@ -11,6 +11,37 @@ class ExpensesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('expenses')->insert([
+        'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+        'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+
+        'expense_date' => '2016-08-01',
+        'amount' => '30',
+        'category_id' => '3',
+        'user_id'=> '2',
+      ]);
+
+      DB::table('expenses')->insert([
+        'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+        'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+
+        'expense_date' => '2016-09-01',
+        'amount' => '12.16',
+        'category_id' => '5',
+        'user_id'=> '2',
+      ]);
+
+      DB::table('expenses')->insert([
+        'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+        'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+
+        'expense_date' => '2016-09-24',
+        'amount' => '9.07',
+        'category_id' => '1',
+        'user_id'=> '1',
+      ]);
+
+
+
     }
 }
