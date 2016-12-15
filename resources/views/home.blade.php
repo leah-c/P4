8868c9c29c7fdcb5922b690610ec052c7898115f
LeahC <chan.leah@gmail.com>
2016-12-15 15:35:43 -0500

    code clean up: removed unnecessary code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,15 +28,6 @@ such as a page specific stylesheets.
     <li class="expenseOp"><a href="#">Update an Expense</a></li>
     <li class="expenseOp"><a href="#">View Expenses</a></li>
   </ul>
-  <!--<h2>Lorem Ipsum Text Generator</h2>
-  <p>The Lorem Ipsum Text Generator tool will give you some dummy filler text. </p>
-  <a href ="generate_ipsum">Let's generate some Lorem Ipsum text!</a>
-
-
-  <h2>Random User Generator</h2>
-  <p>The Random User Generator tool will give you some dummy user names.</p>
-  <a href ="generate_users">Let's generate some random users!</a>
--->
 
 @endsection
 
