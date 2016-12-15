415fcc8e7f54426193beae717dbfe3d4e4080983
LeahC <chan.leah@gmail.com>
2016-12-14 22:35:42 -0500

    created seeder classes

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    DB::table('expenses')->insert([
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+
+      'name' => 'Jill',
+      'email' => 'jill@harvard.edu',
+      'password' => helloworld,
+    ]);
+
+    DB::table('expenses')->insert([
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+
+      'name' => 'Jamal',
+      'email' => 'jamal@harvard.edu',
+      'password' => helloworld,
+    ]);
+
+    DB::table('expenses')->insert([
+      'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+      'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+      
+      'name' => 'Leah Chan',
+      'email' => 'chan.leah@gmail.com',
+      'password' => helloworld,
+    ]);
+  }
+}
