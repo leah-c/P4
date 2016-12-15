803b12d878dc96ce8474e707f10972e9a537be11
LeahC <chan.leah@gmail.com>
2016-12-14 22:06:01 -0500

    created migration: create_categories_table

---
+++ database/migrations/2016_12_15_030255_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('categories', function (Blueprint $table) {
+
+        # Increments method will make a Primary, Auto-Incrementing field.
+        # Most tables start off this way
+        $table->increments('id');
+
+        # This generates two columns: `created_at` and `updated_at` to
+        # keep track of changes to a row
+        $table->timestamps();
+        #$table->timestamp('created_at')->useCurrent = true;
+
+        # The rest of the fields...
+        $table->string('category_name');
+        $table->integer('active')->default(1);
+
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
