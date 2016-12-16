988439dbf35d1389fcd9ea7f76869465176bca14
LeahC <chan.leah@gmail.com>
2016-12-16 13:44:29 -0500

    modified logic to show table only if there are expenses to be displayed

--- resources/views/view_expenses.blade.php
+++ resources/views/view_expenses.blade.php
@@ -18,51 +18,54 @@ Expense Tracker
 
   @section('content')
 
-    <h2>Expense Tracker</h2>
+  <h2>Expense Tracker</h2>
 
-    <!-- will be used to show any messages -->
-    @if (Session::has('message'))
-        <div class="alert alert-success">{{ Session::get('message') }}</div>
+  <!-- will be used to show any messages -->
+  @if (Session::has('message'))
+  <div class="alert alert-success">{{ Session::get('message') }}</div>
+  @endif
+
+  <a href="/expenses/create" class="btn btn-sm btn-primary" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a>
+  <!--<p> Select from one of the following options. </p>
+  <ul>
+  <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a></li>
+  <li class="expenseOp"><a href="#" class="btn btn-sm btn-danger btn-block" id="delete_expense"><span class="glyphicon glyphicon-remove"></span> Delete an Expense</a></li>
+  <li class="expenseOp"><a href="#" class="btn btn-sm btn-warning btn-block" id="edit_expense"><span class="glyphicon glyphicon-edit"></span> Edit an Expense</a></li>
+  <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="view_expense"><span class="glyphicon glyphicon-th-list"></span> View Expenses</a></li>
+</ul>
+-->
+
+@if (isset($expenses) && $expenses->count() > 0)
+{{--
+{{dump($expenses)}}
+{{dump($expenses->count())}}
+--}}
+<table data-toggle="table" class="table table-striped">
+  <thead>
+    <tr>
+      <th>Expense Date</th>
+      <th>Amount</th>
+      <th>Description</th>
+      <th>Category</th>
+      <th>Edit</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($expenses as $expense)
+    <tr>
+      <td>{{$expense->expense_date}}</td>
+      <td>${{$expense->amount}}</td>
+      <td>{{$expense->description}}</td>
+      <td>{{$expense->category_id}}</td>
+      <td><a href="#" class="btn btn-sm btn-warning " id="edit_expense"><span class="glyphicon glyphicon-edit"></span></a>
+        <td><a href="#" class="btn btn-sm btn-danger " id="remove_expense"><span class="glyphicon glyphicon-remove"></span></a>
+        </tr>
+        @endforeach
+
+      </tbody>
+    </table>
     @endif
-
-    <a href="/expenses/create" class="btn btn-sm btn-primary" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a>
-    <!--<p> Select from one of the following options. </p>
-    <ul>
-    <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a></li>
-    <li class="expenseOp"><a href="#" class="btn btn-sm btn-danger btn-block" id="delete_expense"><span class="glyphicon glyphicon-remove"></span> Delete an Expense</a></li>
-    <li class="expenseOp"><a href="#" class="btn btn-sm btn-warning btn-block" id="edit_expense"><span class="glyphicon glyphicon-edit"></span> Edit an Expense</a></li>
-    <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="view_expense"><span class="glyphicon glyphicon-th-list"></span> View Expenses</a></li>
-  </ul>
-  -->
-
-    @if (isset($expenses))
-
-      <table data-toggle="table" class="table table-striped">
-        <thead>
-          <tr>
-            <th>Expense Date</th>
-            <th>Amount</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($expenses as $expense)
-            <tr>
-              <td>{{$expense->expense_date}}</td>
-              <td>${{$expense->amount}}</td>
-              <td>{{$expense->description}}</td>
-              <td>{{$expense->category_id}}</td>
-              <td><a href="#" class="btn btn-sm btn-warning " id="edit_expense"><span class="glyphicon glyphicon-edit"></span></a>
-              <td><a href="#" class="btn btn-sm btn-danger " id="remove_expense"><span class="glyphicon glyphicon-remove"></span></a>
-            </tr>
-          @endforeach
-
-            </tbody>
-          </table>
-      @endif
 
     @endsection
 
