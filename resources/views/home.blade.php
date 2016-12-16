fe52441ca691d0ef3bef915a5058c90fe1468435
LeahC <chan.leah@gmail.com>
2016-12-16 00:04:01 -0500

    added code to format a list of expenses in view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,51 +18,49 @@ Expense Tracker
 
   @section('content')
 
-  <p>Welcome to the expense tracker tool menu!</p>
+    <p>Welcome to the expense tracker tool menu!</p>
 
-  <h2>Let's Get Started!</h2>
+    <h2>Let's Get Started!</h2>
 
-  <a href="#" class="btn btn-sm btn-primary" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a>
-  <!--<p> Select from one of the following options. </p>
-  <ul>
+    <a href="#" class="btn btn-sm btn-primary" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a>
+    <!--<p> Select from one of the following options. </p>
+    <ul>
     <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="add_expense"><span class="glyphicon glyphicon-plus"></span> Add an Expense</a></li>
     <li class="expenseOp"><a href="#" class="btn btn-sm btn-danger btn-block" id="delete_expense"><span class="glyphicon glyphicon-remove"></span> Delete an Expense</a></li>
     <li class="expenseOp"><a href="#" class="btn btn-sm btn-warning btn-block" id="edit_expense"><span class="glyphicon glyphicon-edit"></span> Edit an Expense</a></li>
     <li class="expenseOp"><a href="#" class="btn btn-sm btn-primary btn-block" id="view_expense"><span class="glyphicon glyphicon-th-list"></span> View Expenses</a></li>
   </ul>
--->
-  <!--
-  @foreach($expenses as $expense)
-  <h2>{{$expense->expense_date}}</h2>
-  <p>{{$expense->amount}}</p>
-  @endforeach
--->
-<table data-toggle="table" class="table table-striped">
-  <thead>
-    <tr>
-      <th>Expense Date</th>
-      <th>Amount</th>
-      <th>Description</th>
-      <th>Category</th>
-      <th>Edit</th>
-      <th>Delete</th>
-    </tr>
-  </thead>
-  <tbody>
+  -->
 
-    @foreach($expenses as $expense)
-      <tr>
-        <td>{{$expense->expense_date}}</td>
-        <td>${{$expense->amount}}</td>
-        <td>{{$expense->description}}</td>
-        <td>{{$expense->category_id}}</td>
-        <td><a href="#" class="btn btn-sm btn-warning " id="add_expense"><span class="glyphicon glyphicon-edit"></span></a>
-        <td><a href="#" class="btn btn-sm btn-danger " id="add_expense"><span class="glyphicon glyphicon-remove"></span></a>
-      </tr>
-    @endforeach
+    @if (isset($expenses))
 
-      </tbody>
-    </table>
+      <table data-toggle="table" class="table table-striped">
+        <thead>
+          <tr>
+            <th>Expense Date</th>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($expenses as $expense)
+            <tr>
+              <td>{{$expense->expense_date}}</td>
+              <td>${{$expense->amount}}</td>
+              <td>{{$expense->description}}</td>
+              <td>{{$expense->category_id}}</td>
+              <td><a href="#" class="btn btn-sm btn-warning " id="edit_expense"><span class="glyphicon glyphicon-edit"></span></a>
+              <td><a href="#" class="btn btn-sm btn-danger " id="remove_expense"><span class="glyphicon glyphicon-remove"></span></a>
+            </tr>
+          @endforeach
+
+            </tbody>
+          </table>
+      @endif
+
     @endsection
 
 
