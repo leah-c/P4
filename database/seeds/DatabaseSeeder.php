8703f1f159795c33e7a1f25c7f4a5b8756bdb425
LeahC <chan.leah@gmail.com>
2016-12-16 16:20:02 -0500

    removed empty space

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,5 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(ExpensesTableSeeder::class);
-
-
     }
 }
