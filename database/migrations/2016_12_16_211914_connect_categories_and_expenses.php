21169b8327de600e315002dd0008297a4464e0e2
LeahC <chan.leah@gmail.com>
2016-12-16 16:23:32 -0500

    added foreign key relation to categories table

--- database/migrations/2016_12_16_211914_connect_categories_and_expenses.php
+++ database/migrations/2016_12_16_211914_connect_categories_and_expenses.php
@@ -6,23 +6,27 @@ use Illuminate\Database\Migrations\Migration;
 
 class ConnectCategoriesAndExpenses extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        //
-    }
+  /**
+  * Run the migrations.
+  *
+  * @return void
+  */
+  public function up()
+  {
+    Schema::table('expenses', function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+      # This field `author_id` is a foreign key that connects to the `id` field in the `authors` table
+      $table->foreign('category_id')->references('id')->on('categories');
+      
+  }
+
+  /**
+  * Reverse the migrations.
+  *
+  * @return void
+  */
+  public function down()
+  {
+    Schema::drop('expenses');
+  }
 }
