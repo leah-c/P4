4253c83c17633c359b47ca4df68591eec832f039
LeahC <chan.leah@gmail.com>
2016-12-15 19:48:01 -0500

    code clean up

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -8,16 +8,6 @@ use DB;
 class ExpenseController extends Controller
 {
   /**
-  * Display a listing of the resource.
-  *
-  * @return \Illuminate\Http\Response
-  */
-  public function index()
-  {
-    //
-  }
-
-  /**
   * Show the form for creating a new resource.
   *
   * @return \Illuminate\Http\Response
