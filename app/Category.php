a9985c6faf4f16a32dea8cd32f375767b1ea9666
LeahC <chan.leah@gmail.com>
2016-12-16 17:19:24 -0500

    added relationships and functions

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    
+  public function categories() {
+    # Category has many Expenses
+    # Define a one-to-many relationship.
+    return $this->hasMany('App\Expense');
+  }
+
+  public static function getForDropdown() {
+
+    # Categories
+    $categories = Category::orderBy('category_name', 'ASC')->get();
+
+    $categories_for_dropdown = [];
+    foreach($categories as $category) {
+      $categories_for_dropdown[$category->id] = $category->category_name;
+    }
+
+    return $categories_for_dropdown;
+  }
 }
