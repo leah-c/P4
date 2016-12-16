aaed16f2b39c9106f276bf3038cde989eeea964e
LeahC <chan.leah@gmail.com>
2016-12-16 17:20:14 -0500

    updated relationship with Category in Expense model

--- app/Expense.php
+++ app/Expense.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
+  /* Relationship Methods */
 
+  /**
+  *
+  */
+  public function expense() {
+    # Book belongs to Author
+    # Define an inverse one-to-many relationship.
+    return $this->belongsTo('App\User');
+  }
+
+
+  /*public function user() {
+    return $this->belongsTo('App\User');
+  }*/
+
+  /* End Relationship Methods */
 }
