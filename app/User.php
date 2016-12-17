6b41fa3d6b07d3faa3dc88589218a94783f2c0e5
LeahC <chan.leah@gmail.com>
2016-12-17 16:24:23 -0500

    added relationship to Expense

--- app/User.php
+++ app/User.php
@@ -7,23 +7,42 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /* Relationship Methods */
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+  *
+  */
+  public function expenses() {
+    # User has many Expenses
+    # Define a one-to-many relationship.
+    return $this->hasMany('App\Expense');
+  }
+
+  /* End Relationship Methods */
+
+
+  /**
+  *
+  */
+
+
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
+
+  /**
+  * The attributes that should be hidden for arrays.
+  *
+  * @var array
+  */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 }
