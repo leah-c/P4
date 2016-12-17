975d4aa09e582ba26634d7686de0a3fc7b56831e
LeahC <chan.leah@gmail.com>
2016-12-16 19:50:07 -0500

    modified edit() to pass array of categories to be displayed in edit view

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Expense;
+use App\Category;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
@@ -63,83 +64,91 @@ class ExpenseController extends Controller
       $expense->description = Input::get('description');
     };
 
-    # check to see if a category was selected
-    /*if (isset( $_POST['category_id']) && $_POST['category_id'] != '') {
-      $expense->category_id = Input::get('category_id');
-    };
-    */
-    $expense->save();
 
-    #$expenses = Expense::orderBy('expense_date','descending')->get();
+  $expense->save();
 
-    // redirect
-    Session::flash('message', 'Successfully created a new expense!');
-    #$return Redirect::to('/home')->with(['expenses'=>$expenses]);
-    #return Redirect::to('/home'); //LeahC 12/16
-    return Redirect::to('/expenses'); //LeahC 12/16
+  // redirect
+  Session::flash('message', 'Successfully created a new expense!');
+  return Redirect::to('/expenses');
+}
+
+
+/**
+* Display the specified resource.
+*
+* @param  int  $id
+* @return \Illuminate\Http\Response
+*/
+public function show($id)
+{
+  $expense = Expense::find($id);
+
+  if(is_null($expense)) {
+    Session::flash('message','Expense not found');
+    return redirect('/expenses/home');
   }
 
+  return view('expenses.show')->with('expense', $id);
+}
 
-  /**
-  * Display the specified resource.
-  *
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function show($id)
-  {
-    $expense = Expense::find($id);
+/**
+* Show the form for editing the specified resource.
+*
+* @param  int  $id
+* @return \Illuminate\Http\Response
+*/
+public function edit($id)
+{
+  #TO DO
+  # Validate that it is a good $id being passed in
+  $expense = Expense::find($id);
 
-    if(is_null($expense)) {
-        Session::flash('message','Expense not found');
-        return redirect('/expenses/home');
-    }
+  #Category
+  $categories = Category::orderBy('category_name', 'ASC')->get();
 
-    return view('expenses.show')->with('expense', $id);
+  #dump($categories);
+
+  $categories_for_dropdown = [];
+
+  foreach($categories as $category){
+    $categories_for_dropdown[$category->id]=$category->category_name;
   }
 
-  /**
-  * Show the form for editing the specified resource.
-  *
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function edit($id)
-  {
-    #TO DO
-    # Validate that it is a good $id being passed in
-    $expense = Expense::find($id);
-    #dump($expense);
-    return view('edit_expense')->with(['expense'=>$expense]);
-  }
+  return view('edit_expense')->with(
+    [
+      'expense'=>$expense,
+      'categories_for_dropdown' => $categories_for_dropdown,
+    ]
+  );
+}
 
-  /**
-  * Update the specified resource in storage.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function update(Request $request, $id)
-  {
-    # Validate
-    $this->validate($request, [
-      'expense_date' => 'required | date',
-      'amount' => 'required | numeric',
-      'category_id' => 'required',
-      'description' => 'max:50',
-    ]);
+/**
+* Update the specified resource in storage.
+*
+* @param  \Illuminate\Http\Request  $request
+* @param  int  $id
+* @return \Illuminate\Http\Response
+*/
+public function update(Request $request, $id)
+{
+  # Validate
+  $this->validate($request, [
+    'expense_date' => 'required | date',
+    'amount' => 'required | numeric',
+    'category_id' => 'required',
+    'description' => 'max:50',
+  ]);
 
-    # Find and update expense
-    $expense = Expense::find($request->id);
-    $expense->expense_date = $request->expense_date;
-    $expense->amount = $request->amount;
-    $expense->category_id= $request->category_id;
+  # Find and update expense
+  $expense = Expense::find($request->id);
+  $expense->expense_date = $request->expense_date;
+  $expense->amount = $request->amount;
+  $expense->category_id= $request->category_id;
 
-    # check to see if an expense desc was created
-    if (isset( $_POST['description']) && $_POST['description'] != '') {
-      $expense->description = $request->description;
-    };
+  # check to see if an expense desc was created
+  if (isset( $_POST['description']) && $_POST['description'] != '') {
+    $expense->description = $request->description;
+  };
 
   $expense->save();
 
