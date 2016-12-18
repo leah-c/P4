cf36708e0382a58cd0293653b584bdb64463e9f3
LeahC <chan.leah@gmail.com>
2016-12-17 20:06:27 -0500

    moved flash message to master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,43 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>
-        {{-- Yield the title if it exists, otherwise default to 'Foobooks' --}}
-        @yield('title', 'Expense Tracker')
-    </title>
+  <title>
+    {{-- Yield the title if it exists, otherwise default to 'Foobooks' --}}
+    @yield('title', 'Expense Tracker')
+  </title>
 
-    <meta charset='utf-8'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <meta charset='utf-8'>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+  integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" >
+  <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" >
 
-    <link href="/css/style.css" type='text/css' rel='stylesheet'>
-    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
-    {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
+  <link href="/css/style.css" type='text/css' rel='stylesheet'>
+  <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+  {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
     @yield('head')
 
-</head>
-<body>
+  </head>
+  <body>
 
-  <div class="container">
-    <header>
-      <!--<h1 id="app_name">Expense Tracker</h1>-->
-    </header>
+    <!-- will be used to show any messages -->
 
-    <section>
+
+    <div class="container">
+      <header>
+        <!--<h1 id="app_name">Expense Tracker</h1>-->
+      </header>
+
+      <section>
+        @if (Session::has('message'))
+          <div class="alert alert-success">{{ Session::get('message') }}</div>
+        @endif
+        
         {{-- Main page content will be yielded here --}}
         @yield('content')
-    </section>
+      </section>
 
-    <footer>
-    </footer>
+      <footer>
+      </footer>
 
-    {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
-    @yield('body')
-  </div>
+      {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
+      @yield('body')
+    </div>
 
-</body>
-</html>
+  </body>
+  </html>
