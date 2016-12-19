f996aa49816bab72982068a76d50dff91e4ed73e
LeahC <chan.leah@gmail.com>
2016-12-19 01:22:10 -0500

    override Auth() routes, code clean up

--- routes/web.php
+++ routes/web.php
@@ -10,35 +10,17 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
 # Direct user to the Expense Tracker sign in/registration page
 Route::get('/', 'IndexController@index')->name('expense_tracker.index');
-
-# Direct user to the Expense Tracker sign in/registration page
-Route::post('/', 'AccountController@validateAccount')->name('expense_tracker.index');
-
-# Direct user to the Expense Tracker landing page after adding and expense
-Route::get('/home', 'IndexController@homepage')->name('expense_tracker.home')->middleware('auth');
-
-# Direct user to the Expense Tracker landing page after account registration/ sign in
-# will show expense table if user has any associated with their id
-Route::post('/home', 'IndexController@homepage')->name('expense_tracker.home');
-
-#Direct user to the registration form page
-Route::get('/register_new_account', 'AccountController@registerAccount')->name('new_account.store');
-
-# Show all expenses
-Route::get('/expenses', 'ExpenseController@index')->name('expense.index');
 
 # Show all expenses after log in
 Route::post('/expenses/home', 'ExpenseController@index')->name('expense.index');
 
-# Show all expenses when user clicks cancel upin editin or adding an expense
+# Show all expenses when user clicks cancel upon editin or adding an expense
 Route::get('/expenses/home', 'ExpenseController@index')->name('expense.index');
 
 # Show a form to add an expense
 Route::get('/expenses/create', 'ExpenseController@create')->name('expense.create');
-#Route::get('/expense/create', 'ExpenseController@create')->name('expenses.create')->middleware('auth');
 
 # Process the form to add a new expense
 Route::post('/expenses', 'ExpenseController@store')->name('expense.store');
@@ -55,22 +37,11 @@ Route::put('/expenses/{id}', 'ExpenseController@update')->name('expense.update')
 # Get route to confirm deletion of an expense
 Route::get('/expenses/{id}/delete', 'ExpenseController@delete')->name('expense.delete');
 
-# Delete route to actually destroy the book
+# Delete route to actually destroy the expense
 Route::delete('/expenses/{id}', 'ExpenseController@destroy')->name('expense.destroy');
 
 Auth::routes();
+
+Route::post('/login', 'IndexController@validateLogin')->name('login');
+
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
-Route::get('/home', 'HomeController@index');
-
-Route::get('/show-login-status', function() {
-
-  # You may access the authenticated user via the Auth facade
-  $user = Auth::user();
-
-  if($user)
-  dump($user->toArray());
-  else
-  dump('You are not logged in.');
-
-  return;
-});
